d0ec40b3fa2dcd9e23fdc294b9756a0ef093d5e7
Aaron Moreno Villeda <hh1360@hotmail.com>
2022-08-14 08:49:27 -0500

    Create 010.c

---
+++ 010.c
@@ -0,0 +1,16 @@
+/*
+*Desarrollador: Aaron Moreno Villeda
+*
+*Fecha de creacion: 14/08/22
+*
+*10.-Generar un algortimo que permita obtener una cantidad N de números pseudoaleatorios (donde N es un número entero, mayor a 0) 
+*utilzando para ello el método/algoritmo de Generación de números congruencial.
+*/
+
+#include<stdio.h>
+
+int main()
+{
+  
+  return 0;
+}
